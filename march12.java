ddabbbc4150a15c1f1626146f6fe93dbac4c6f41
khushalhingorani1304 <khushalhingorani1304@gmail.com>
2024-03-13 14:25:16 +0530

    1

---
+++ march12.java
@@ -0,0 +1,30 @@
+// Java program to illustrate 
+// Generics 
+public class march12<T> { 
+    // private T value; 
+    
+    //     public march12(T value) 
+    //     { 
+    //         this.value = value; 
+    //     } 
+    
+    //     public void showType() 
+    //     { 
+    //         System.out.println("Type:" + 
+    //             value.getClass().getSimpleName()); 
+    //         System.out.println("Value: " + value); 
+    //     } 
+    
+    //     public static void main(String[] args) 
+    //     { 
+    //         march12<String> Str = 
+    //         new march12<String>("Generics vs Templates"); 
+    
+    //         march12<Integer> integer = 
+    //                         new march12<Integer>(9); 
+    
+    //         Str.showType(); 
+    //         integer.showType(); 
+    //     } 
+    } 
+    
---
+++ march13.java
@@ -0,0 +1,11 @@
+// !COLLECTIoN  FRAMEWORK
+// collection - INTERFACE;
+// collections - CLASS
+
+// *COllection in java is a framework that provide an architecture to store and manipulate the grp of objects
+// *JAVA COLLECTION FRAMEWORK PROVIDE MANY INTERFACE(Set, List, Queue, DeQueue and Map) and Classes(Array, LinkedList, )
+
+public class march13 {
+
+    
+}
